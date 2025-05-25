eec17439d6f219762e6151603d70b9f7f5ddad11
mediareason <brottler@gmail.com>
2025-05-25 14:41:05 -0700

    Remove conflicting script files

--- script-part3.js
+++ script-part3.js
@@ -1,3 +1,1 @@
-// This file has been merged into script.js
-// Keeping for reference only
-console.log('⚠️ script-part3.js loaded - this should not be used anymore');
+// DELETE - This file is removed to fix conflicts
